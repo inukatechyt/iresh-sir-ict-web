f0a9a1c732a38856e490e00d10071df2ecd8b92d
Inuka Sanjula <154017084+inukatechyt@users.noreply.github.com>
2026-09-03 10:52:10 +0530

    db 7

--- dashboard.js
+++ dashboard.js
@@ -1,7 +1,6 @@
 // 1. Supabase සම්බන්ධ කිරීම
 const SUPABASE_URL = 'https://ercowsldngxxzpvpevxa.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyY293c2xkbmd4eHpwdnBldnhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNjA1NjksImV4cCI6MjEwMzkzNjU2OX0.BGFibu7_xRZUl9c2rkH3KA-y0kJsm8iCA2YLtnRwn9o
-';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyY293c2xkbmd4eHpwdnBldnhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNjA1NjksImV4cCI6MjEwMzkzNjU2OX0.BGFibu7_xRZUl9c2rkH3KA-y0kJsm8iCA2YLtnRwn9o';
 
 // මෙතන නම supabaseClient කියලා වෙනස් කළා
 const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
