593613aaaddf37c5e2c142cd15333af5f65dc8d5
Inuka Sanjula <154017084+inukatechyt@users.noreply.github.com>
2026-09-04 11:48:00 +0530

    sdfgbdfhdf

--- admin.js
+++ admin.js
@@ -1,3 +1,8 @@
+// Supabase Connection (සම්පූර්ණයෙන්ම අලුතින් සහ නිවැරදිව සම්බන්ධ කිරීම)
+const SUPABASE_URL = 'https://ercowsldngxxzpvpevxa.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVyY293c2xkbmd4eHpwdnBldnhhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgzNjA1NjksImV4cCI6MjEwMzkzNjU2OX0.BGFibu7_xRZUl9c2rkH3KA-y0kJsm8iCA2YLtnRwn9o';
+
+const supabaseClient = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 // ==========================================
 // 1. ADMIN CONFIG & SECURITY
 // ==========================================
